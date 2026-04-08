4b61263ace39928fa88dbeec9368d49e9e822aa0
Melinacrz01-maker <melinacrz01@gmail.com>
2026-04-08 10:13:56 -0300

    cambio de interfaz

--- ProyectoForms.cs/frmGestionProductos.Designer.cs
+++ ProyectoForms.cs/frmGestionProductos.Designer.cs
@@ -36,18 +36,18 @@
             this.lblMoney = new System.Windows.Forms.Label();
             this.lblCategory = new System.Windows.Forms.Label();
             this.lbldescription = new System.Windows.Forms.Label();
-            this.textBX1 = new System.Windows.Forms.TextBox();
-            this.combitobox = new System.Windows.Forms.ComboBox();
+            this.textPrecio = new System.Windows.Forms.TextBox();
+            this.comboboxcategoria = new System.Windows.Forms.ComboBox();
             this.texpaquete = new System.Windows.Forms.TextBox();
-            this.txtCd = new System.Windows.Forms.TextBox();
-            this.textBoxYourname = new System.Windows.Forms.TextBox();
-            this.textBDS = new System.Windows.Forms.TextBox();
+            this.textCodigo = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textboxDescrepcion = new System.Windows.Forms.TextBox();
             this.btnGrabar = new System.Windows.Forms.Button();
-            this.pictureBoxDOS = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Imagendos = new System.Windows.Forms.PictureBox();
+            this.ImagenUno = new System.Windows.Forms.PictureBox();
             this.buttmodifique = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDOS)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Imagendos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenUno)).BeginInit();
             this.SuspendLayout();
             // 
             // lblG
@@ -57,11 +57,11 @@
             this.lblG.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblG.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblG.ForeColor = System.Drawing.Color.Black;
-            this.lblG.Location = new System.Drawing.Point(86, 9);
+            this.lblG.Location = new System.Drawing.Point(63, 9);
             this.lblG.Name = "lblG";
-            this.lblG.Size = new System.Drawing.Size(191, 24);
+            this.lblG.Size = new System.Drawing.Size(265, 24);
             this.lblG.TabIndex = 1;
-            this.lblG.Text = "Gestion de Productos";
+            this.lblG.Text = "Gestión de Productos Petshop";
             // 
             // lblname
             // 
@@ -81,7 +81,7 @@
             this.lblcode.Name = "lblcode";
             this.lblcode.Size = new System.Drawing.Size(61, 16);
             this.lblcode.TabIndex = 3;
-            this.lblcode.Text = "Codigo:";
+            this.lblcode.Text = "Código:";
             // 
             // lblStockk
             // 
@@ -111,7 +111,7 @@
             this.lblCategory.Name = "lblCategory";
             this.lblCategory.Size = new System.Drawing.Size(79, 16);
             this.lblCategory.TabIndex = 6;
-            this.lblCategory.Text = "Categoria:";
+            this.lblCategory.Text = "Categoría:";
             // 
             // lbldescription
             // 
@@ -123,22 +123,27 @@
             this.lbldescription.TabIndex = 7;
             this.lbldescription.Text = "Descripción:";
             // 
-            // textBX1
+            // textPrecio
             // 
-            this.textBX1.BackColor = System.Drawing.Color.BlanchedAlmond;
-            this.textBX1.Location = new System.Drawing.Point(125, 141);
-            this.textBX1.Name = "textBX1";
-            this.textBX1.Size = new System.Drawing.Size(60, 20);
-            this.textBX1.TabIndex = 3;
+            this.textPrecio.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.textPrecio.Location = new System.Drawing.Point(125, 141);
+            this.textPrecio.Name = "textPrecio";
+            this.textPrecio.Size = new System.Drawing.Size(60, 20);
+            this.textPrecio.TabIndex = 3;
             // 
-            // combitobox
+            // comboboxcategoria
             // 
-            this.combitobox.BackColor = System.Drawing.Color.BlanchedAlmond;
-            this.combitobox.FormattingEnabled = true;
-            this.combitobox.Location = new System.Drawing.Point(125, 173);
-            this.combitobox.Name = "combitobox";
-            this.combitobox.Size = new System.Drawing.Size(141, 21);
-            this.combitobox.TabIndex = 4;
+            this.comboboxcategoria.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.comboboxcategoria.FormattingEnabled = true;
+            this.comboboxcategoria.Items.AddRange(new object[] {
+            "Alimentos",
+            "Juguetes",
+            "Ropa",
+            "Collares "});
+            this.comboboxcategoria.Location = new System.Drawing.Point(125, 173);
+            this.comboboxcategoria.Name = "comboboxcategoria";
+            this.comboboxcategoria.Size = new System.Drawing.Size(141, 21);
+            this.comboboxcategoria.TabIndex = 4;
             // 
             // texpaquete
             // 
@@ -148,30 +153,31 @@
             this.texpaquete.Size = new System.Drawing.Size(60, 20);
             this.texpaquete.TabIndex = 2;
             // 
-            // txtCd
+            // textCodigo
             // 
-            this.txtCd.BackColor = System.Drawing.Color.BlanchedAlmond;
-            this.txtCd.Location = new System.Drawing.Point(125, 81);
-            this.txtCd.Name = "txtCd";
-            this.txtCd.Size = new System.Drawing.Size(141, 20);
-            this.txtCd.TabIndex = 1;
+            this.textCodigo.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.textCodigo.Location = new System.Drawing.Point(125, 81);
+            this.textCodigo.Name = "textCodigo";
+            this.textCodigo.Size = new System.Drawing.Size(141, 20);
+            this.textCodigo.TabIndex = 1;
+            this.textCodigo.TextChanged += new System.EventHandler(this.textCodigo_TextChanged);
             // 
-            // textBoxYourname
+            // textBoxNombre
             // 
-            this.textBoxYourname.BackColor = System.Drawing.Color.BlanchedAlmond;
-            this.textBoxYourname.Location = new System.Drawing.Point(125, 52);
-            this.textBoxYourname.Name = "textBoxYourname";
-            this.textBoxYourname.Size = new System.Drawing.Size(141, 20);
-            this.textBoxYourname.TabIndex = 0;
+            this.textBoxNombre.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.textBoxNombre.Location = new System.Drawing.Point(125, 52);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(141, 20);
+            this.textBoxNombre.TabIndex = 0;
             // 
-            // textBDS
+            // textboxDescrepcion
             // 
-            this.textBDS.BackColor = System.Drawing.Color.BlanchedAlmond;
-            this.textBDS.Location = new System.Drawing.Point(125, 215);
-            this.textBDS.Multiline = true;
-            this.textBDS.Name = "textBDS";
-            this.textBDS.Size = new System.Drawing.Size(141, 81);
-            this.textBDS.TabIndex = 5;
+            this.textboxDescrepcion.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.textboxDescrepcion.Location = new System.Drawing.Point(125, 215);
+            this.textboxDescrepcion.Multiline = true;
+            this.textboxDescrepcion.Name = "textboxDescrepcion";
+            this.textboxDescrepcion.Size = new System.Drawing.Size(141, 81);
+            this.textboxDescrepcion.TabIndex = 5;
             // 
             // btnGrabar
             // 
@@ -186,26 +192,26 @@
             this.btnGrabar.UseVisualStyleBackColor = false;
             this.btnGrabar.Click += new System.EventHandler(this.bttcargar_Click);
             // 
-            // pictureBoxDOS
+            // Imagendos
             // 
-            this.pictureBoxDOS.Image = global::ProyectoForms.cs.Properties.Resources.yellow_fancy_dog_puppy_avatar_badge_poster_logo_templates_print_illustration_in_flat_cartoon_style_vector__1_;
-            this.pictureBoxDOS.Location = new System.Drawing.Point(192, 331);
-            this.pictureBoxDOS.Name = "pictureBoxDOS";
-            this.pictureBoxDOS.Size = new System.Drawing.Size(122, 118);
-            this.pictureBoxDOS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxDOS.TabIndex = 15;
-            this.pictureBoxDOS.TabStop = false;
+            this.Imagendos.Image = global::ProyectoForms.cs.Properties.Resources.yellow_fancy_dog_puppy_avatar_badge_poster_logo_templates_print_illustration_in_flat_cartoon_style_vector__1_;
+            this.Imagendos.Location = new System.Drawing.Point(192, 331);
+            this.Imagendos.Name = "Imagendos";
+            this.Imagendos.Size = new System.Drawing.Size(122, 118);
+            this.Imagendos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Imagendos.TabIndex = 15;
+            this.Imagendos.TabStop = false;
             // 
-            // pictureBox1
+            // ImagenUno
             // 
-            this.pictureBox1.BackgroundImage = global::ProyectoForms.cs.Properties.Resources.yellow_fancy_dog_puppy_avatar_badge_poster_logo_templates_print_illustration_in_flat_cartoon_style_vector;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.ErrorImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(28, 331);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(113, 118);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.ImagenUno.BackgroundImage = global::ProyectoForms.cs.Properties.Resources.yellow_fancy_dog_puppy_avatar_badge_poster_logo_templates_print_illustration_in_flat_cartoon_style_vector;
+            this.ImagenUno.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ImagenUno.ErrorImage = null;
+            this.ImagenUno.Location = new System.Drawing.Point(28, 331);
+            this.ImagenUno.Name = "ImagenUno";
+            this.ImagenUno.Size = new System.Drawing.Size(113, 118);
+            this.ImagenUno.TabIndex = 0;
+            this.ImagenUno.TabStop = false;
             // 
             // buttmodifique
             // 
@@ -226,14 +232,14 @@
             this.BackColor = System.Drawing.Color.Gold;
             this.ClientSize = new System.Drawing.Size(340, 510);
             this.Controls.Add(this.buttmodifique);
-            this.Controls.Add(this.pictureBoxDOS);
+            this.Controls.Add(this.Imagendos);
             this.Controls.Add(this.btnGrabar);
-            this.Controls.Add(this.textBDS);
-            this.Controls.Add(this.textBoxYourname);
-            this.Controls.Add(this.txtCd);
+            this.Controls.Add(this.textboxDescrepcion);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textCodigo);
             this.Controls.Add(this.texpaquete);
-            this.Controls.Add(this.combitobox);
-            this.Controls.Add(this.textBX1);
+            this.Controls.Add(this.comboboxcategoria);
+            this.Controls.Add(this.textPrecio);
             this.Controls.Add(this.lbldescription);
             this.Controls.Add(this.lblCategory);
             this.Controls.Add(this.lblMoney);
@@ -241,14 +247,14 @@
             this.Controls.Add(this.lblcode);
             this.Controls.Add(this.lblname);
             this.Controls.Add(this.lblG);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ImagenUno);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmGestionProductos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gestión de productos - PETSHOP";
             this.Load += new System.EventHandler(this.frmGestionProductos_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDOS)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Imagendos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagenUno)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -256,7 +262,7 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox ImagenUno;
         private System.Windows.Forms.Label lblG;
         private System.Windows.Forms.Label lblname;
         private System.Windows.Forms.Label lblcode;
@@ -264,14 +270,14 @@
         private System.Windows.Forms.Label lblMoney;
         private System.Windows.Forms.Label lblCategory;
         private System.Windows.Forms.Label lbldescription;
-        private System.Windows.Forms.TextBox textBX1;
-        private System.Windows.Forms.ComboBox combitobox;
+        private System.Windows.Forms.TextBox textPrecio;
+        private System.Windows.Forms.ComboBox comboboxcategoria;
         private System.Windows.Forms.TextBox texpaquete;
-        private System.Windows.Forms.TextBox txtCd;
-        private System.Windows.Forms.TextBox textBoxYourname;
-        private System.Windows.Forms.TextBox textBDS;
+        private System.Windows.Forms.TextBox textCodigo;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textboxDescrepcion;
         private System.Windows.Forms.Button btnGrabar;
-        private System.Windows.Forms.PictureBox pictureBoxDOS;
+        private System.Windows.Forms.PictureBox Imagendos;
         private System.Windows.Forms.Button buttmodifique;
     }
 }
